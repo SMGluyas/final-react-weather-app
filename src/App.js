72b9ea31661f29b00cdf25625d998265ce37e3c3
SMGluyas <74235270+SMGluyas@users.noreply.github.com>
2021-01-08 23:00:29 +1300

    Linked Github to project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1 className="App-header">
         Weather App
       </h1>
+      <footer>This project was coded by SG <a href="https://github.com/SMGluyas/final-react-weather-app" target="_blank" rel="noreferrer">open sourced</a> on Github</footer>
     </div>
   );
 }
