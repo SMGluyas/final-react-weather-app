85976616265d91a7e08f2278583ae3cca0a2708b
SMGluyas <74235270+SMGluyas@users.noreply.github.com>
2021-01-09 12:00:36 +1300

    imported bootstrap into index.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
       <h1 className="App-header">
         Weather App
       </h1>
-      <button className="btn button-success">Hello World</button>
+      <button className="btn btn-success">Hello World</button>
       <footer>This project was coded by SG <a href="https://github.com/SMGluyas/final-react-weather-app" target="_blank" rel="noreferrer">open sourced</a> on Github</footer>
     </div>
   );
